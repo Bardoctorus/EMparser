9d99745fe118e370dbba4575a4438b152cf64a71
Bardoctorus <ian.m.buckley@gmail.com>
2022-02-10 16:59:55 +0100

    added rest of libsyn desc and youtube timestamps

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ filepath = 'input/ems072.txt'
 if len(sys.argv) > 1:
     epno = int(sys.argv[1])
     yturl = str(sys.argv[2])
+    intro = str(sys.argv[3])
 else:
     sys.exit("usage: python3 parser.py EPISODENUMBER YOUTUBEURL")
 print(epno)
@@ -28,8 +29,32 @@ def makeYT(lines):
         ytf.write(text.strip('\n')+ ': '+link)
         counter+=3
 
+    ytf.write("----------------------------------------------------------i\n")
+    counter = 1
+    while(counter < len(lines)-1):
+        text = str(lines[counter-1]).strip('\n')
+        number = lines [counter+1]
+        ytf.write(text+' '+number)
+        counter+=3
+
 # function for libsyn
 def makeLS(lines):
+    pre = ('<p>This week\'s Electromaker Show is now available on YouTube and everywhere you get your podcasts!</p>\
+<p><span style="font-weight: 400;">Welcome to the Electromaker Show episode {}! {}</span></p>\
+<p>Tune in for the latest maker, tech, DIY, IoT, embedded, and crowdfunding news stories from the week.</p>\
+<p>&nbsp;</p>\
+<p><iframe title="YouTube video player" src="https://www.youtube.com/embed/{}" width="560" height="315" frameborder="0"></iframe></p>\
+<p><span style="font-weight: 400;"><a href="https://www.youtube.com/watch?v={}">Watch the show!</a></span></p>\
+<p><span style="font-weight: 400;">We publish a new show every week. Subscribe here:</span> <a href="https://www.youtube.com/channel/UCiMO2NHYWNiVTzyGsPYn4DA?sub_confirmation=1"> <span style="font-weight: 400;">https://www.youtube.com/channel/UCiMO2NHYWNiVTzyGsPYn4DA?sub_confirmation=1</span></a></p>\
+<p><span style="font-weight: 400;">We stock the latest products from Adafruit, Seeed Studio, Pimoroni, Sparkfun, and many more! Browse our shop:</span></p>\
+<p><a href="https://www.youtube.com/redirect?event=video_description&amp;redir_token=QUFFLUhqbVI1SkM3TXE2Z3g4MzhqUHRUTFVkZVlBSEx0QXxBQ3Jtc0trOFhxMS1kLXdtQWNrYzFwMlhRN05Jako3WVgteVB3YWQ1MXg0YjNQVXdRRDJJeHNxVmZnTmF4cnpjYlZXQzRkUW9SRXN6cFVDTDBtR2pjLXVrYmtLR3EyekFvOC1INnE0Uk1OdDJTSXBmbXBqNTdkcw&amp;q=https%3A%2F%2Fwww.electromaker.io%2Fshop"> <span style="font-weight: 400;">https://www.electromaker.io/shop</span></a></p>\
+<p><span style="font-weight: 400;">Join us on Discord!</span> <a href="https://discord.com/invite/w8d7mkCkxj%E2%80%8B"><span style="font-weight: 400;">https://discord.com/invite/w8d7mkCkxj​</span></a></p>\
+<p><span style="font-weight: 400;">Follow us on Twitter:</span> <a href="https://twitter.com/ElectromakerIO"><span style="font-weight: 400;">https://twitter.com/ElectromakerIO</span></a></p>\
+<p><span style="font-weight: 400;">Like us on Facebook:</span> <a href="https://www.facebook.com/electromaker.io/"><span style="font-weight: 400;">https://www.facebook.com/electromaker.io/</span></a></p>\
+<p><span style="font-weight: 400;">Follow us on Instagram:</span> <a href="https://www.instagram.com/electromaker_io/"><span style="font-weight: 400;">https://www.instagram.com/electromaker_io/</span></a></p>\
+<p>Featured in this show:</p>\
+<ul>'.format(str(epno),str(intro), str(yturl),str(yturl)))
+    lsf.write(pre)
     counter = 1
     while(counter < len(lines)-1):
         text = lines[counter-1]
@@ -37,7 +62,9 @@ def makeLS(lines):
 
         lsf.write('<li><a href="'+str(link)+'">'+str(text)+'</a></li>'+'\n')
         counter+=3
+    
 
+    lsf.write('</ul>')
 
 
 # function for mailchimp
